afabd2bc6bebd740d9bd6f0db24529be9148d6a3
Mathias Kahl <mathias.kahl@googlemail.com>
2011-08-07 00:38:15 +0200

    UnitTests begonnen

---
+++ Base Tests/SceneImporterTest.cs
@@ -0,0 +1,82 @@
+﻿using ColladaXna.Base.Import;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Xml;
+using ColladaXna.Base;
+
+namespace Base_Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for SceneImporterTest and is intended
+    ///to contain all SceneImporterTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class SceneImporterTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Import
+        ///</summary>
+        [TestMethod()]
+        public void ImportTest()
+        {
+            SceneImporter target = new SceneImporter(); // TODO: Initialize to an appropriate value
+            XmlNode xmlRoot = null; // TODO: Initialize to an appropriate value
+            ColladaModel model = null; // TODO: Initialize to an appropriate value
+            target.Import(xmlRoot, model);
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+    }
+}
